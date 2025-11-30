e5fa9889ea4a7f3ccd818ae8edb1c7bf48545365
Kristján Valur Jónsson <sweskman@gmail.com>
2025-11-30 20:45:02 +0000

    Convert tests to pytest style
    - Replaced unittest.TestCase with plain classes
    - Converted self.assertEqual/assertNotEqual/assertTrue to assert statements
    - Renamed test methods from testX to test_x (pytest naming convention)
    - Removed unittest.main() boilerplate
    - Fixed bug in stub_new() where 'dup' should be 'stub'
    - Changed import from unittest to pytest
    
    Tests now run with: pytest tests/test_tealet.py

--- tests/test_tealet.py
+++ tests/test_tealet.py
@@ -1,5 +1,4 @@
-
-import unittest
+import pytest
 import math
 
 import _tealet
@@ -27,7 +26,7 @@ def stub_new(func=None, arg=None, klass=_tealet.tealet, retarg=False):
         r = stub.run(func, arg)
     else:
         r = None
-    return (dup, r) if retarg else dup
+    return (stub, r) if retarg else stub
 
 def stub_new2(func=None, arg=None, klass=_tealet.tealet, retarg=False):
     stub = tealet_new_descend(random.randint(0, 20), klass=klass, retarg=False)
@@ -59,107 +58,107 @@ def get_new():
     return newarray(random.randint(0, len(newarray)-1))
 
 
-class TestModule(unittest.TestCase):
-    def testMain(self):
-        self.assertEqual(_tealet.main(), _tealet.current())
+class TestModule:
+    def test_main(self):
+        assert _tealet.main() == _tealet.current()
 
-    def testMain2(self):
-        self.assertEqual(_tealet.main(), _tealet.current().main)
+    def test_main2(self):
+        assert _tealet.main() == _tealet.current().main
 
-    def testMain3(self):
-        self.assertEqual(_tealet.main().state, _tealet.STATE_RUN)
+    def test_main3(self):
+        assert _tealet.main().state == _tealet.STATE_RUN
 
-class TestSimple(unittest.TestCase):
-    def testSimple(self):
+class TestSimple:
+    def test_simple(self):
         status = [0]
         def run(current, arg):
             status[0] = 1
             return arg
         get_new()(run, _tealet.current())
-        self.assertEqual(status[0], 1)
+        assert status[0] == 1
 
-class TestStatus(unittest.TestCase):
-    def testStatusRun(self):
+class TestStatus:
+    def test_status_run(self):
         t = _tealet.current()
-        self.assertEqual(t.main, _tealet.main())
-        self.assertEqual(t.state, _tealet.STATE_RUN)
+        assert t.main == _tealet.main()
+        assert t.state == _tealet.STATE_RUN
 
-    def testStatusStub(self):
+    def test_status_stub(self):
         stub = get_new()()
         status = [None]
-        self.assertEqual(stub.state, _tealet.STATE_STUB)
+        assert stub.state == _tealet.STATE_STUB
         def run(current, arg):
             status[0] = current.state
             return arg
         stub.run(run, _tealet.current())
-        self.assertEqual(status[0], _tealet.STATE_RUN)
+        assert status[0] == _tealet.STATE_RUN
 
-class TestSubclass(unittest.TestCase):
+class TestSubclass:
     class sc(_tealet.tealet):
         dude = [0]
         def __repr__(self):
             return "<myrepr %r>"%super(TestSubclass.sc, self).__repr__()
         def __del__(self):
             self.dude[0] = 1
-    def testSubclass(self):
+    def test_subclass(self):
         def foo(current, arg):
             arg.switch(current)
             return arg
         t = get_new()(foo, _tealet.current(), klass=self.sc)
-        self.assertEqual(repr(t)[:7], "<myrepr")
-        self.assertEqual(self.sc.dude[0], 0)
+        assert repr(t)[:7] == "<myrepr"
+        assert self.sc.dude[0] == 0
         t.switch()
-        self.assertEqual(self.sc.dude[0], 0)
+        assert self.sc.dude[0] == 0
         del t
-        self.assertEqual(self.sc.dude[0], 1)
+        assert self.sc.dude[0] == 1
 
-class TestSwitch(unittest.TestCase):
-    def testSwitch(self):
+class TestSwitch:
+    def test_switch(self):
         status = [0]
         t = [None, None]
         def t2(current, arg):
-            self.assertNotEqual(current, _tealet.main())
-            self.assertNotEqual(current, t[0])
+            assert current != _tealet.main()
+            assert current != t[0]
             t[1] = current
-            self.assertEqual(status[0], 1)
+            assert status[0] == 1
             status[0] = 2
-            self.assertEqual(_tealet.current(), current)
+            assert _tealet.current() == current
             t[0].switch()
-            self.assertEqual(status[0], 3)
+            assert status[0] == 3
             status[0] = 4
-            self.assertEqual(_tealet.current(), current)
+            assert _tealet.current() == current
             t[0].switch()
-            self.assertEqual(status[0], 5)
+            assert status[0] == 5
             status[0] = 6
-            self.assertEqual(current, t[1])
-            self.assertEqual(_tealet.current(), current)
+            assert current == t[1]
+            assert _tealet.current() == current
             t[1].switch() #noop
-            self.assertEqual(status[0], 6)
+            assert status[0] == 6
             status[0] = 7
-            self.assertEqual(_tealet.current(), current)
+            assert _tealet.current() == current
             return _tealet.main()
 
         def t1(current, arg):
-            self.assertNotEqual(current, _tealet.main())
+            assert current != _tealet.main()
             t[0] = current
-            self.assertEqual(status[0], 0)
+            assert status[0] == 0
             status[0] = 1
-            self.assertEqual(current, _tealet.current())
+            assert current == _tealet.current()
             get_new()(t2)
-            self.assertEqual(status[0], 2)
+            assert status[0] == 2
             status[0] = 3
-            self.assertEqual(current, _tealet.current())
+            assert current == _tealet.current()
             t[1].switch()
-            self.assertEqual(status[0], 4)
+            assert status[0] == 4
             status[0] = 5
-            self.assertEqual(current, _tealet.current())
+            assert current == _tealet.current()
             return t[1]
 
         get_new()(t1)
-        self.assertEqual(status[0], 7)
+        assert status[0] == 7
 
 
-    def testSwitchNew(self):
+    def test_switch_new(self):
         # 1 is high on the stack.  We then create 2 lower on the stack
         # the execution is : m 1 m 2 1 m 2 m */
         def new1(current, arg):
@@ -181,16 +180,16 @@ class TestSwitch(unittest.TestCase):
         # the tealet is now running
         tealet2 = tealet_new_descend(4, new2, tealet1)
 
-        self.assertEqual(tealet2.state, _tealet.STATE_RUN);
+        assert tealet2.state == _tealet.STATE_RUN;
         tealet2.switch()
 
-    def testSwitchArg(self):
+    def test_switch_arg(self):
         # 1 is high on the stack.  We then create 2 lower on the stack
         # the execution is : m 1 m 2 1 m 2 m */
         def new1(current, arg):
             # switch back to the creator
             r = arg.switch(2)
-            self.assertEqual(r, 4)
+            assert r == 4
             # now we want to trample the stack
             stub = tealet_new_descend(50)
             del stub
@@ -200,22 +199,22 @@ class TestSwitch(unittest.TestCase):
         def new2(current, arg):
             # switch to tealet 1 to trample the stack
             r = arg.switch(4);
-            self.assertEqual(r, 6)
+            assert r == 6
             # back to main
             return _tealet.main(), 7
 
         tealet1, r = get_new()(new1, _tealet.current(), retarg=True)
-        self.assertEqual(r, 2)
+        assert r == 2
         # the tealet is now running
         tealet2, r = tealet_new_descend(4, new2, tealet1, retarg=True)
-        self.assertEqual(r, 5)
+        assert r == 5
 
-        self.assertEqual(tealet2.state, _tealet.STATE_RUN);
+        assert tealet2.state == _tealet.STATE_RUN;
         r = tealet2.switch(6)
-        self.assertEqual(r, 7)
+        assert r == 7
 
 
-class TestRandom1(unittest.TestCase):
+class TestRandom1:
     max_status = 10000
 
     def randomRun(self, index):
@@ -236,61 +235,61 @@ class TestRandom1(unittest.TestCase):
                 #print 'switch', i
                 d = self.tealets[i].switch()
                 #assert d == math.sqrt(2344.2)
-            self.assertTrue(self.status >= prevstatus)
-            self.assertEqual(_tealet.current(), cur)
-            self.assertEqual(self.tealets[index], cur)
-            self.assertEqual(self.got_index, index)
+            assert self.status >= prevstatus
+            assert _tealet.current() == cur
+            assert self.tealets[index] == cur
+            assert self.got_index == index
             if self.status >= self.max_status:
                 break
 
     def randomTealet(self, current, index):
         i = self.got_index;
-        self.assertEqual(_tealet.current(), current)
-        self.assertEqual(i, index)
-        self.assertTrue(i > 0 and i < len(self.tealets))
-        self.assertEqual(self.tealets[i], None)
+        assert _tealet.current() == current
+        assert i == index
+        assert i > 0 and i < len(self.tealets)
+        assert self.tealets[i] == None
         self.tealets[i] = current
         self.randomRun(i)
         self.tealets[i] = None
 
         i = random.randint(0, len(self.tealets)-1)
         if not self.tealets[i]:
-            self.assertTrue(self.tealets[0]);
+            assert self.tealets[0];
             i = 0
         self.got_index = i
         #print "ret", i
         return self.tealets[i]
 
-    def testRandom(self):
+    def test_random(self):
         self.tealets = [None]*127
         self.status = 0
         self.tealets[0] = _tealet.current()
         while self.status < self.max_status:
             self.randomRun(0)
 
-        self.assertEqual(_tealet.current(), self.tealets[0])
+        assert _tealet.current() == self.tealets[0]
         for i in range(1, len(self.tealets)):
             while self.tealets[i]:
                 self.randomRun(0)
 
 
-class TestRandom2(unittest.TestCase):
+class TestRandom2:
     MAX_STATUS = 10000
     N_RUNS = 10
     MAX_DESCEND = 20
     ARRAYSIZE = 127
 
     def randomTealet(self, cur, index):
-        self.assertEqual(_tealet.current(), cur)
-        self.assertTrue(index > 0 and index < len(self.tealets))
-        self.assertEqual(self.tealets[index], None)
+        assert _tealet.current() == cur
+        assert index > 0 and index < len(self.tealets)
+        assert self.tealets[index] == None
         self.tealets[index] = cur
         self.randomRun(index)
         self.tealets[index] = None
         return self.tealets[0] # switch to main
 
     def randomRun(self, index):
-        self.assertTrue(self.tealets[index] == None or self.tealets[index] == _tealet.current())
+        assert self.tealets[index] == None or self.tealets[index] == _tealet.current()
         self.tealets[index] = _tealet.current()
         for i in range(self.N_RUNS):
             if self.randomDescend(index, random.randint(0, self.MAX_DESCEND+1)) == 0:
@@ -319,7 +318,7 @@ class TestRandom2(unittest.TestCase):
                     return 1
             return 0
 
-    def testRandom(self):
+    def test_random(self):
         self.tealets = [None] * self.ARRAYSIZE
         self.status = 0
         self.tealets[0] = _tealet.current()
@@ -343,14 +342,4 @@ class TestRandom2(unittest.TestCase):
 
 
 
-if __name__ == '__main__':
-    import sys
-    if not sys.argv[1:]:
-        sys.argv.append('-v')
-
-    for i in range(0, len(newarray)):
-        newmode = i
-        unittest.main()
-    newmode = -1
-    unittest.main()
-
+# Tests can be run with: pytest tests/test_tealet.py
