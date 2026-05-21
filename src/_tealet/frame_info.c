13394052baea56c3a6e7a319052315c79e810bdf
Kristján Valur Jónsson <sweskman@gmail.com>
2026-05-21 20:44:19 +0000

    small fix

--- src/_tealet/frame_info.c
+++ src/_tealet/frame_info.c
@@ -26,9 +26,10 @@ void PyTealetFrameInfo_Init(PyTealetFrameInfo *info) {
 
 void PyTealetFrameInfo_Fini(PyTealetFrameInfo *info) {
 #if defined(PYTEALET_HAS_PENDING_FRAME_INTROSPECTION) && defined(PY312P)
-    if (info->items != info->fixed_items)
+    if (info->items != info->fixed_items) {
         PyMem_Free(info->items);
         info->items = info->fixed_items;
+    }
 #else
     (void)info;
 #endif
